b4dcf476ba85396e7238e7ca89f5ff409728b897
James Ots <code@jamesots.com>
2014-01-09 10:52:29 +0000

    Added the ability to read xml from a stream, instead of from a string.

--- lib/xml/xml_streamer.dart
+++ lib/xml/xml_streamer.dart
@@ -3,6 +3,7 @@ part of xml_stream;
 class XmlStreamer {
   static const EMPTY = '';
   
+  Stream<List<int>> stream;
   String raw;
   String _open_value;
   
@@ -12,84 +13,113 @@ class XmlStreamer {
   
   XmlStreamer(this.raw);
   
+  XmlStreamer.fromStream(this.stream);
+  
   Stream<XmlEvent> read() {
     _controller = new StreamController<XmlEvent>();
-    
     XmlEvent event = createAndAddXmlEvent(XmlState.StartDocument);
+  
     String prev;
-    var chars_raw = this.raw.split("");
-    for (var ch in chars_raw) {
-      switch(ch) {
-        case XmlChar.LT:
-          if (event.state != null && event.value.trim().isNotEmpty) {
-            _addElement(event);
-          }
-          event = _createXmlEvent(XmlState.Open);
-          break;
-        case XmlChar.GT:
-          if (prev == XmlChar.SLASH) {
-            if ((event.value.length -1) == event.value.lastIndexOf("/")) {
-              event.value = event.value.substring(0, event.value.lastIndexOf("/"));
-            }
-            event = _createXmlEventAndCheck(event, XmlState.Closed);
-            event.value = _open_value;
-          }
-          _addElement(event);
-          event = _createXmlEvent(XmlState.Text);
-          break;
-        case XmlChar.SLASH:
-          if (event.state == XmlState.Open) { 
-            event = _createXmlEvent(XmlState.Closed);
-          } else {
-            event = addCharToValue(event, ch);
-          }
-          break;
-        case XmlChar.SPACE:
-          if (event.state == XmlState.Open && event.value == '!--') {
-            event = _createXmlEvent(XmlState.Comment);
-          } else if (event.state == XmlState.Open || event.state == XmlState.Attribute) {
-            _addElement(event);
-            event = _createXmlEvent(event.state);
-            event.fired = true;
-          } else {
-            event = addCharToValue(event, ch);
-          }
-          break;
-        case XmlChar.EQUALS:
-          var value = event.value;
-          if (event.state == XmlState.Open || event.state == XmlState.Attribute) {
-            event = _createXmlEventAndCheck(event, XmlState.Attribute);
-            event.key = value;
-          } else {
-            event.value = "$value$ch";
-          }
-          break;
-        case XmlChar.DASH:
-          if (event.state != XmlState.Comment) {
-            event = addCharToValue(event, ch);
-          }
-          break;
-        case XmlChar.QUESTIONMARK:
-          if (prev == XmlChar.LT || event.state == XmlState.Top || event.state == XmlState.StartDocument) {
-            event = _createXmlEvent(XmlState.Top);
-          } else {
-            event = addCharToValue(event, ch);
-          }
-          break;
-        case XmlChar.SINGLE_QUOTES:
-          break;
-        case XmlChar.DOUBLE_QUOTES:
-          break;
-        case XmlChar.NEWLINE:
-          break;
-        default:
-          event = addCharToValue(event, ch);
+    if (this.stream == null) {
+      var chars_raw = this.raw.split("");
+      for (var ch in chars_raw) {
+        event = _processRawChar(ch, prev, event);
+        prev = ch;
+        if (_shutdown) break;
       }
-      prev = ch;
-      if (_shutdown) break;
+      createAndAddXmlEvent(XmlState.EndDocument);
+      _controller.close();
+    } else {
+      StreamSubscription controller;
+      var onData = (data) {
+        var chars_raw = new String.fromCharCodes(data).split("");
+        for (var ch in chars_raw) {
+          event = _processRawChar(ch, prev, event);
+          prev = ch;
+          if (_shutdown) {
+            controller.cancel();
+            break;
+          }
+        }
+      };
+      controller = stream.listen(onData,
+          onDone: () {
+            createAndAddXmlEvent(XmlState.EndDocument);
+            _controller.close();
+          });
     }
-    event = createAndAddXmlEvent(XmlState.EndDocument);
+
     return _controller.stream;
+  }
+  
+  XmlEvent _processRawChar(var ch, var prev, XmlEvent event) {
+    switch(ch) {
+      case XmlChar.LT:
+        if (event.state != null && event.value.trim().isNotEmpty) {
+          _addElement(event);
+        }
+        event = _createXmlEvent(XmlState.Open);
+        break;
+      case XmlChar.GT:
+        if (prev == XmlChar.SLASH) {
+          if ((event.value.length -1) == event.value.lastIndexOf("/")) {
+            event.value = event.value.substring(0, event.value.lastIndexOf("/"));
+          }
+          event = _createXmlEventAndCheck(event, XmlState.Closed);
+          event.value = _open_value;
+        }
+        _addElement(event);
+        event = _createXmlEvent(XmlState.Text);
+        break;
+      case XmlChar.SLASH:
+        if (event.state == XmlState.Open) { 
+          event = _createXmlEvent(XmlState.Closed);
+        } else {
+          event = addCharToValue(event, ch);
+        }
+        break;
+      case XmlChar.SPACE:
+        if (event.state == XmlState.Open && event.value == '!--') {
+          event = _createXmlEvent(XmlState.Comment);
+        } else if (event.state == XmlState.Open || event.state == XmlState.Attribute) {
+          _addElement(event);
+          event = _createXmlEvent(event.state);
+          event.fired = true;
+        } else {
+          event = addCharToValue(event, ch);
+        }
+        break;
+      case XmlChar.EQUALS:
+        var value = event.value;
+        if (event.state == XmlState.Open || event.state == XmlState.Attribute) {
+          event = _createXmlEventAndCheck(event, XmlState.Attribute);
+          event.key = value;
+        } else {
+          event.value = "$value$ch";
+        }
+        break;
+      case XmlChar.DASH:
+        if (event.state != XmlState.Comment) {
+          event = addCharToValue(event, ch);
+        }
+        break;
+      case XmlChar.QUESTIONMARK:
+        if (prev == XmlChar.LT || event.state == XmlState.Top || event.state == XmlState.StartDocument) {
+          event = _createXmlEvent(XmlState.Top);
+        } else {
+          event = addCharToValue(event, ch);
+        }
+        break;
+      case XmlChar.SINGLE_QUOTES:
+        break;
+      case XmlChar.DOUBLE_QUOTES:
+        break;
+      case XmlChar.NEWLINE:
+        break;
+      default:
+        event = addCharToValue(event, ch);
+    }
+    return event;
   }
   
   XmlEvent _addElement(XmlEvent event) {
--- test/hierarchical_test.dart
+++ test/hierarchical_test.dart
@@ -1,27 +1,50 @@
 import 'package:unittest/unittest.dart';
 import 'package:xmlstream/xmlstream.dart';
+import 'dart:io';
+import 'dart:async';
 
 main() {  
-  // First tests!  
-  var rawText = '''<?xml version="1.0" encoding="UTF-8"?>
-                      <hello attr="flow">
-                        <item>
-                          <subitem>internal</subitem>
-                          <subitem>hello</subitem>
-                        </item>
-                        <item>This is a sentence!</item>
-                       </hello>''';
-    
-    var states = [XmlState.StartDocument, XmlState.Top ,XmlState.Open, XmlState.Attribute, XmlState.Open, XmlState.Open, XmlState.Text, XmlState.Closed, XmlState.Open, XmlState.Text, XmlState.Closed, XmlState.Closed, XmlState.Open, XmlState.Text, XmlState.Closed, XmlState.Closed, XmlState.EndDocument];
-    var values = ["", "","hello","flow","item","subitem","internal","subitem","subitem","hello","subitem","item","item","This is a sentence!","item","hello",""];
-    int count = 0;
-    
-    var xmlStreamer = new XmlStreamer(rawText);
-    xmlStreamer.read().listen((e) {
-      test('basic xml streaming $e', () {
-        expect(states[count], e.state);
-        expect(values[count], e.value);
-        count++;
+  group('reading xml', () {
+    // First tests!  
+    var rawText = '''<?xml version="1.0" encoding="UTF-8"?>
+                        <hello attr="flow">
+                          <item>
+                            <subitem>internal</subitem>
+                            <subitem>hello</subitem>
+                          </item>
+                          <item>This is a sentence!</item>
+                         </hello>''';
+      
+      var states = [XmlState.StartDocument, XmlState.Top ,XmlState.Open, XmlState.Attribute, XmlState.Open, XmlState.Open, XmlState.Text, XmlState.Closed, XmlState.Open, XmlState.Text, XmlState.Closed, XmlState.Closed, XmlState.Open, XmlState.Text, XmlState.Closed, XmlState.Closed, XmlState.EndDocument];
+      var values = ["", "","hello","flow","item","subitem","internal","subitem","subitem","hello","subitem","item","item","This is a sentence!","item","hello",""];
+      int count = 0;
+      
+      test('using a string', () {
+        var c = new Completer();
+        var xmlStreamer = new XmlStreamer(rawText);
+        xmlStreamer.read().listen((e) {
+          expect(e.state, states[count]);
+          expect(e.value, values[count]);
+          count++;
+        }, onDone: () {
+          c.complete();
+        });
+        return c.future;
       });
-    });
+      
+      test('using a stream', () {
+        var c = new Completer();
+        var streamCount = 0;
+        var stream = new File("test1.xml").openRead(); 
+        var xmlStreamer = new XmlStreamer.fromStream(stream);
+        xmlStreamer.read().listen((e) {
+          expect(e.state, states[streamCount]);
+          expect(e.value, values[streamCount]);
+          streamCount++;
+        }, onDone: () {
+          c.complete();
+        });
+        return c.future;
+      });
+  });
 }
