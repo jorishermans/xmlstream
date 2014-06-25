30d480f7c9468524367a59b53b553482ee84a80a
joris hermans <JHermans85@hotmail.com>
2014-06-25 09:34:50 +0200

    better testing

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+### Changelog ###
+
+This file contains highlights of what changes on each version of the forcemvc package. 
+
+#### Pub version 0.9.0 ####
+
+- Add better parsing for comments
+- Adding tests and implementation for CDATA
+
+#### Pub version 0.8.15 ####
+
+Adding functionality to read a file
--- test/comments_test.dart
+++ test/comments_test.dart
@@ -3,10 +3,10 @@ import 'package:xmlstream/xmlstream.dart';
 
 main() {  
   // First tests!  
-  var rawText = '<?xml version="1.0" encoding="UTF-8"?><!-- dit is een comment --><hello attr="flow">world</hello>';
+  var rawText = '<?xml version="1.0" encoding="UTF-8"?><!--dit is een comment --><hello attr="flow">world</hello>';
     
     var states = [XmlState.StartDocument, XmlState.Top, XmlState.Comment, XmlState.Open, XmlState.Attribute, XmlState.Text, XmlState.Closed, XmlState.EndDocument];
-    var values = ["", ""," dit is een comment ", "hello", "flow","world", "hello", ""];
+    var values = ["", "","dit is een comment ", "hello", "flow","world", "hello", ""];
     int count = 0;
     
     var xmlStreamer = new XmlStreamer(rawText);
